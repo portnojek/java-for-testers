c8b3860fd5404d18ece9bf402b4100d0d44aed2d
Your Name <bartosz.kociecki@gmail.com>
2026-09-20 11:39:42 +0200

    update

---
+++ src/main/java/Exercise.java
@@ -0,0 +1,26 @@
+public class Exercise {
+    public int timeJump() {
+        int initialYear = 1985;
+
+        // Create a new variable called timeJump
+        // Add it to the initialYear and assign the result to targetYear
+        // so that the targetYear becomes 2015
+
+        int targetYear = 0;
+
+        // TODO: Your code goes here
+        int timeJump = 30;
+        targetYear = initialYear + timeJump;
+        return targetYear;
+    }
+
+    public String toLowerCase(String text) {
+        text = text.toLowerCase();
+        return text;
+    }
+
+    public String trimText(String text) {
+        text = text.trim();
+        return text;
+    }
+}
--- src/test/java/HelloWorldWriterTest.java
+++ src/test/java/HelloWorldWriterTest.java
@@ -2,9 +2,46 @@ import org.junit.jupiter.api.Test;
 
 public class HelloWorldWriterTest {
     @Test
-    public void shouldWriteHelloWorldToTheConsole(){
+    public void shouldWriteHelloWorldToTheConsole() {
         HelloWorldWriter writer = new HelloWorldWriter();
         writer.writeHelloWorld();
+    }
+
+    @Test
+    public void declaringNUmercialVariables() {
+        final int ageThisYear = 42;
+        int ageNextYear = ageThisYear + 1;
+        long starsInTheGalaxy = 4200000000L;
+        double weight = 15.5;
+        System.out.println(ageNextYear);
+        System.out.printf("Age next year: %d\n", ageNextYear);
+        System.out.printf("Stars in the galaxy: %d\n", starsInTheGalaxy);
+        System.out.println(weight);
+    }
+
+    @Test
+    public void workingWithStrings() {
+        String firstName = "Sarah-Jane\n";
+        String upperCaseFirstName = firstName.toUpperCase();
+        String replacedName = firstName.replace("Sarah", "Mary");
+        System.out.printf(upperCaseFirstName);
+        System.out.printf(replacedName);
+
+    }
+
+    @Test
+    public void timeJump() {
+        int initialYear = 1985;
+
+        // Create a new variable called timeJump
+        // Add it to the initialYear and assign the result to targetYear
+        // so that the targetYear becomes 2015
+
+        int targetYear = 0;
+        // TODO: Your code goes here
+        int timeJump = 40;
+        targetYear = initialYear + timeJump;
+        //return targetYear; => testy nie mogą zwracać wartości
     }
 
 }
